fd55c20d2f2e93e0087e26994df2cfc11f87c279
siva <sksivaiah759@gmail.com>
2023-04-22 10:05:35 +0000

    done

---
+++ index.js
@@ -0,0 +1,13 @@
+const element = (
+  // Write your code here.
+  <div className="oo">
+    <h1 className="sk">Congratulations</h1>
+    <div className="ok">
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png" />
+      <h2>Siva</h2>
+      <p>ok ok</p>
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
